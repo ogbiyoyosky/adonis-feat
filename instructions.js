d096da6f01c640164fdb07198b10e7ccb2f724cc
Emmanuel Ogbiyoyo <emmanuel.o@crennettechlabs.com>
2020-05-01 08:04:29 +0100

    instruction update

--- instructions.js
+++ instructions.js
@@ -12,6 +12,7 @@ const path = require('path')
 
 module.exports = async cli => {
     try {
+        console.log("running ")
         await cli.copy(path.join(__dirname, 'templates/BaseFeature.js'), path.join(cli.helpers.appPath(), 'Features/BaseFeature.js'))
         cli.command.completed('create', 'Features/BaseFeature.js')
     } catch (error) {
